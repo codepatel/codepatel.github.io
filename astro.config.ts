f4d483d2eb1c6b6ae67d919d59f787925899fee6
aitorllamas <aitorllj93@gmail.com>
2025-08-09 16:47:02 +0200

    refactor: update integration setup for astroSpaceship in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,11 @@
 
 import { defineConfig } from 'astro/config';
-import { create } from 'astro-spaceship';
+import { astroSpaceship } from 'astro-spaceship';
 
 import websiteConfig from './website.config.json';
 
-export default defineConfig(create(websiteConfig));
+export default defineConfig({
+  integrations: [
+    astroSpaceship(websiteConfig)
+  ]
+});
