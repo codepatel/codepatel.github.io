b2266d41f0201764b0c274e0c2d92795a0fdb6b0
aitorllamas <aitorllj93@gmail.com>
2025-08-30 19:39:34 +0200

    temporary remove sensitive from obsidian vault dir

--- src/collections/documents.ts
+++ src/collections/documents.ts
@@ -2,17 +2,17 @@ import { defineCollection } from 'astro:content';
 
 import { ObsidianMdLoader, ObsidianWikiLinkSchema } from "astro-loader-obsidian";
 
-import { DOCUMENTS_COLLECTION_NAME } from 'astro-spaceship/constants';
+import { DOCUMENTS_COLLECTION_NAME, DEFAULT_VAULT_DIR } from 'astro-spaceship/constants';
 import { DocumentSchema } from 'astro-spaceship/schemas';
-
 import config from 'astro-spaceship/config';
 
+import { ENV } from 'varlock/env';
 
 export default {
   [DOCUMENTS_COLLECTION_NAME]: defineCollection({
     loader: ObsidianMdLoader({
       author: config.author,
-      base: config.vaultDir,
+      base: ENV.OBSIDIAN_VAULT_DIR ?? DEFAULT_VAULT_DIR,
       url: '',
       wikilinkFields: ['relateds']
     }),
